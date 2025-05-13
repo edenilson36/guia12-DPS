ccde82c369baa2c1e42902dd7c3104a81cf9acf6
edenilson36 <cr241530@alumno.udb.edu.sv>
2025-05-12 21:35:29 -0600

    guia12

---
+++ guia12/ejemplo4/class/database.class.php
@@ -0,0 +1,16 @@
+<?php
+// Configuración de la conexión a base de datos
+$bd_host     = "localhost";
+$bd_usuario  = "root";
+$bd_password = "";
+$bd_base     = "empleadosajax";
+
+$con = new mysqli($bd_host, $bd_usuario, $bd_password, $bd_base);
+$con->set_charset("utf8");
+
+if ($con->connect_error) {
+  echo "<p>Se ha producido el error número " . $con->connect_errno .
+       " con la siguiente descripción: " . $con->connect_error . "</p>";
+  exit(0);
+}
+?>
